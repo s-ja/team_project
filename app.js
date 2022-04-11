67bb63e16af9bfcac04692e11cbd37b3073fde3a
안승지 <69342971+s-ja@users.noreply.github.com>
2022-04-11 09:30:53 +0900

    package.json 수정

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express")
-const app = express()
-// const port = 3000
+const express = require("express");
+const app = express();
+const port = 5000
 
 app.use(express.static('public'))
 
